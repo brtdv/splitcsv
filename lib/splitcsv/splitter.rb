b3dcf6467acdc1dcedf22325a31a4469863bb2c0
Bert Devriese <bert.devriese@gmail.com>
2013-09-13 16:36:45 +0200

    Fixes

---
+++ lib/splitcsv/splitter.rb
@@ -0,0 +1,31 @@
+require 'CSV'
+
+module SplitCSV
+  class Splitter
+    def split(file, maxlength=5000, separator=';')
+      csv_basename = File.basename(file, ".*")
+      csv_path     = File.expand_path(file)
+      csv_dir      = File.dirname(csv_path)
+
+      csv_obj      = CSV.open(csv_path, :row_sep => :auto, :col_sep => separator)
+      csv_length   = csv_obj.readlines.size
+
+      # Opnieuw inlezen zodat readline pointer terug aan 0 staat. Na gebruiken van
+      # csv_obj.readlines.size werd "csv_obj" al eens volledig doorlopen
+      csv_obj      = CSV.open(csv_path, :row_sep => :auto, :col_sep => separator)
+      firstline    = csv_obj.readline
+
+      for i in 0..((csv_length / maxlength.to_i).ceil) do
+        CSV.open("#{csv_dir}/#{csv_basename}_#{i+1}.csv", 'w', :row_sep => :auto, :col_sep => separator) do |csv|
+          csv << firstline
+
+          for j in 0..maxlength do
+            if (line = csv_obj.readline) != nil
+              csv << line
+            end
+          end
+        end
+      end
+    end
+  end
+end
